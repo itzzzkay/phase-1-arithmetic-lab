fb5e45180fe98ce2c0067962564e2e0d3cf7efbb
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-03-18 14:48:05 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 10);
+console.log(random);
+
+let num3 = 16;
+let num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(1, 4, 16, 3, 12, 20);
+console.log(max);
